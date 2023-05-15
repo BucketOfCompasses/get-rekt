56f1061afd7db8b296d33a7f4a310ad88c3d4c6f
MessyEllie <messy3llie@gmail.com>
2023-05-15 23:24:57 +0100

    Implement FilterStorage.firstOrNull

--- src/main/kotlin/compasses/getrekt/storage/FilterStorage.kt
+++ src/main/kotlin/compasses/getrekt/storage/FilterStorage.kt
@@ -10,15 +10,25 @@ import compasses.getrekt.filters.Filter
 import compasses.getrekt.filters.FilterAction
 import compasses.getrekt.filters.FilterType
 import compasses.getrekt.filters.MatchType
+import net.codebox.homoglyph.HomoglyphBuilder
 
 object FilterStorage {
+	private val filters: List<Filter> = listOf(
+			Filter("beans", MatchType.CONTAINS, FilterAction.MuteAction, FilterType.TEXT),
+			Filter("ketchup", MatchType.CONTAINS, FilterAction.KickAction, FilterType.TEXT)
+	)
+	private val homoglyphs = HomoglyphBuilder.build()
+
 	fun firstOrNull(raw: String, filterType: FilterType): Filter? {
-		// NOTE: Sort by severity, use the most severe first
-
-		if (raw.contains("beans") && filterType == FilterType.TEXT) {
-			return Filter("beans", MatchType.CONTAINS, FilterAction.MuteAction, FilterType.TEXT)
-		}
-
-		return null
+		return filters.sortedByDescending { it.filterAction.severity }
+				.filter { it.filterType == filterType }
+				.firstOrNull {
+					when (it.matchType) {
+						MatchType.CONTAINS -> homoglyphs.search(raw, it.match.lowercase()).size > 0
+						MatchType.EXACT -> raw.equals(it.match, ignoreCase = true)
+						MatchType.REGEX_CONTAINS -> raw.contains(it.match.toRegex(RegexOption.IGNORE_CASE))
+						MatchType.REGEX -> it.match.toRegex(RegexOption.IGNORE_CASE).matches(raw)
+					}
+				}
 	}
 }
