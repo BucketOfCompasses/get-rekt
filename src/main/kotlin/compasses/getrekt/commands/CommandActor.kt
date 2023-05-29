a5414783afba5fc39393fb72315787643062cbfb
MessyEllie <messy3llie@gmail.com>
2023-05-29 18:54:29 +0100

    Rework temp ban command a little more, factor out time + reason code into it's own class.

---
+++ src/main/kotlin/compasses/getrekt/commands/CommandActor.kt
@@ -0,0 +1,85 @@
+package compasses.getrekt.commands
+
+import com.ibm.icu.text.MeasureFormat
+import com.ibm.icu.util.Measure
+import com.ibm.icu.util.MeasureUnit
+import com.mojang.authlib.GameProfile
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimePeriod
+import kotlinx.datetime.toJavaInstant
+import java.util.*
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+private const val DURATION_REGEX = "([1-9][0-9]*) (years|months|weeks|days|hours|minutes|year|month|week|day|hour|minute)"
+
+class CommandActor(private val targets: Collection<GameProfile>, reasonDurationPair: String) {
+	private val durationFormat = MeasureFormat.getInstance(Locale.ENGLISH, MeasureFormat.FormatWidth.WIDE)
+
+	private val startInstant = Clock.System.now()
+
+	private val duration : DateTimePeriod
+
+	val startDate : Date
+
+	val endDate : Date
+
+	val reason : String
+
+	init {
+		val matches = DURATION_REGEX.toRegex().findAll(reasonDurationPair)
+		var foundDuration : MatchResult? = null
+
+		for (match in matches.toList().reversed()) {
+			if (match.range.first == 0 || match.range.last == reasonDurationPair.length - 1) {
+				foundDuration = match
+				break
+			}
+		}
+
+		if (foundDuration == null) {
+			throw IllegalArgumentException("Invalid input for reasonDurationPair")
+		}
+
+		val durationLength = foundDuration.groupValues[1].toInt()
+
+		duration = when(foundDuration.groupValues.last().lowercase()) {
+			"minute", "minutes" -> DateTimePeriod(minutes = durationLength)
+			"hour", "hours" -> DateTimePeriod(hours = durationLength)
+			"day", "days" -> DateTimePeriod(days = durationLength)
+			"week", "weeks" -> DateTimePeriod(days = durationLength * 7)
+			"month", "months" -> DateTimePeriod(months = durationLength)
+			"year", "years" -> DateTimePeriod(years = durationLength)
+			else -> throw IllegalStateException("Unreachable code.")
+		}
+
+		reason = reasonDurationPair.removeRange(foundDuration.range).trim()
+
+		startDate = Date.from(startInstant.toJavaInstant())
+		endDate = Date.from((startInstant + duration.seconds.toDuration(DurationUnit.SECONDS)).toJavaInstant())
+	}
+
+	fun act(action: (GameProfile) -> Boolean) : Int {
+		var result = 0
+		for (target in targets) {
+			if (action.invoke(target)) {
+				result++
+			}
+		}
+		return result
+	}
+
+	fun durationString(): String {
+		val measures = mutableListOf<Measure>()
+		if (duration.years != 0) measures.add(Measure(duration.years, MeasureUnit.YEAR))
+		if (duration.months != 0) measures.add(Measure(duration.months, MeasureUnit.MONTH))
+		val weeks = Math.floorDiv(duration.days, 7)
+		val days = duration.days - (7 * weeks)
+		if (weeks != 0) measures.add(Measure(weeks, MeasureUnit.WEEK))
+		if (days != 0) measures.add(Measure(days, MeasureUnit.DAY))
+		if (duration.hours != 0) measures.add(Measure(duration.hours, MeasureUnit.HOUR))
+		if (duration.minutes != 0) measures.add(Measure(duration.minutes, MeasureUnit.MINUTE))
+
+		return durationFormat.formatMeasures(*measures.toTypedArray())
+	}
+}
--- src/main/kotlin/compasses/getrekt/commands/TempBanCommand.kt
+++ src/main/kotlin/compasses/getrekt/commands/TempBanCommand.kt
@@ -17,11 +17,9 @@ import net.minecraft.commands.arguments.GameProfileArgument
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.ComponentUtils
 import net.minecraft.server.players.UserBanListEntry
-import java.time.temporal.ChronoUnit
-import java.util.Date
+import java.lang.IllegalArgumentException
 
 object TempBanCommand {
-	private const val DURATION_REGEX = "([1-9][0-9]*) (years|months|days|hours|minutes|year|month|day|hour|minute)"
 	private val ALREADY_BANNED_ERROR = SimpleCommandExceptionType(Component.translatable("commands.ban.failed"))
 
 	fun initialize(dispatcher: CommandDispatcher<CommandSourceStack>) {
@@ -35,55 +33,34 @@ object TempBanCommand {
 						val targets: Collection<GameProfile> = GameProfileArgument.getGameProfiles(this, "targets")
 
 						val reasonDurationPair = StringArgumentType.getString(this, "reason / duration")
-						val matches = DURATION_REGEX.toRegex().findAll(reasonDurationPair)
-						var banDuration : MatchResult? = null
 
-
-
-						for (match in matches.toList().reversed()) {
-							if (match.range.first == 0 || match.range.last == reasonDurationPair.length - 1) {
-								banDuration = match
-								break
-							}
+						val commandActor = try {
+							CommandActor(targets, reasonDurationPair)
+						} catch (exception : IllegalArgumentException) {
+								source.sendSystemMessage(Translations.get("get-rekt.command.failed_find_duration"))
+								return@runs 0
 						}
 
-						if (banDuration == null) {
-							source.sendSystemMessage(Translations.get("get-rekt.command.failed_find_duration"))
-							return@runs 0
-						}
-
-						val banStartDate = Date()
-
-						val duration = banDuration.groupValues[1].toLong()
-
-						val unit = when (banDuration.groupValues.last().lowercase()) {
-							"minute", "minutes" -> ChronoUnit.MINUTES
-							"hour", "hours" -> ChronoUnit.HOURS
-							"day", "days" -> ChronoUnit.DAYS
-							"week", "weeks" -> ChronoUnit.WEEKS
-							"month", "months" -> ChronoUnit.MONTHS
-							"year", "years" -> ChronoUnit.YEARS
-							else -> throw IllegalStateException("Unreachable code.")
-						}
-
-						val banEndDate = Date.from(banStartDate.toInstant().plus(duration, unit))
-
-						val banReason = reasonDurationPair.removeRange(banDuration.range).trim()
-
-						var bans = 0
 						val banList = source.server.playerList.bans
 
-						for (target in targets) {
+						val bans = commandActor.act { target ->
 							if (!banList.isBanned(target)) {
-								val entry = UserBanListEntry(target, banStartDate, source.textName, banEndDate, banReason)
+								val entry = UserBanListEntry(target, commandActor.startDate, source.textName, commandActor.endDate, commandActor.reason)
 								banList.add(entry)
 
-								bans++
-								source.sendSuccess({ Component.translatable("commands.ban.success", ComponentUtils.getDisplayName(target), banReason) }, true)
+								source.sendSuccess({ Component.translatable("commands.ban.success", ComponentUtils.getDisplayName(target), commandActor.reason) }, true)
 
 								val player = source.server.playerList.getPlayer(target.id)
-								player?.connection?.disconnect(Component.translatable("multiplayer.disconnect.banned"))
+
+								val banMessage = if (commandActor.reason.isBlank()) Component.translatable("multiplayer.disconnect.banned") else Component.translatable("multiplayer.disconnect.banned.reason", commandActor.reason)
+								banMessage.append(Translations.get("get-rekt.multiplayer.banned.duration", commandActor.durationString()))
+
+								player?.connection?.disconnect(banMessage)
+
+								return@act true
 							}
+
+							return@act false
 						}
 
 						if (bans == 0) {
